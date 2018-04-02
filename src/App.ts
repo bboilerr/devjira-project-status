6cd2d8f93fb05378c76c603ca06d838801dd0470
Brent Rager <Brent.Rager@genesys.com>
2018-04-02 02:13:43 -0400

    Got report data.

--- src/App.ts
+++ src/App.ts
@@ -1,18 +1,44 @@
-import * as express from 'express'
-import * as csv from 'csv'
-import * as JiraApi from 'jira-client'
-import { Config, Search } from './config'
+import * as JiraApi from 'jira-client';
+import { Config, Search } from './config';
+import * as _ from 'lodash';
+
+interface Sprint {
+    id: number,
+    name: string,
+    state: string,
+    startDate: string,
+    endDate: string,
+    completedDate: string
+}
+
+interface ReportDataIssue {
+    issue: string,
+    uri: string,
+    assignee: string,
+    issueType: string,
+    status: string,
+    summary: string,
+    sprint: Sprint,
+    epic: string,
+    storyPoints: number,
+    storyPointsRemaining: number
+}
+
+interface ReportData {
+    reportDataIssues: Array<ReportDataIssue>,
+}
+
+const ISSUES_PER_SEARCH = 50;
 
 export default class App {
-    private express
-    private jira
-    private jiraFields = {}
-    private jiraFieldsByName = {}
+    private express;
+    private jira;
+    private jiraFields;
+    private jiraIssues;
+    private reportData: ReportData;
 
-    constructor (private config: Config) {
-        this.express = express()
-        this.mountRoutes()
-        this.config = config
+    constructor (private config: Config, private jiraSearch: Search) {
+        this.config = config;
         this.jira = new JiraApi({
             protocol: config.jira.protocol,
             host: config.jira.host,
@@ -23,51 +49,188 @@ export default class App {
         });
     }
 
-    private mountRoutes (): void {
-        const router = express.Router()
-        router.get('/', (req, res) => {
-            res.json({
-                message: 'Hello World!'
-            })
-        })
-        this.express.use('/', router)
-    }
-
     private async getJiraFields() {
         try {
-            let fields = await this.jira.listFields()
-            let fieldsMap = {byId: {}, byName: {}}
+            let fields = await this.jira.listFields();
+            let fieldsMap = {byId: {}, byName: {}};
             for (let field of fields) {
-                fieldsMap.byId[field.id] = field
-                fieldsMap.byName[field.name] = field
+                fieldsMap.byId[field.id] = field;
+                fieldsMap.byName[field.name] = field;
             }
-            return fieldsMap
+            return fieldsMap;
         } catch (error) {
-            console.error(`Error getting Jira Fields: ${error}`)
-            throw error
+            console.error(`Error getting Jira Fields: ${error}`);
+            throw error;
         }
     }
 
-    private async performJiraSearch(search: Search, startAt: number = 0, maxResults: number = 50) {
+    private async performJiraSearch(search: Search, startAt: number = 0, maxResults: number = ISSUES_PER_SEARCH) {
         try {
-            let search = this.config.search[0]
-            console.log(`Performing search: ${search.name}, startAt: ${startAt}, maxResults: ${maxResults}`)
-            return await this.jira.searchJira(search.jql, {startAt: startAt, maxResults: maxResults})
+            let search = this.config.searches[0];
+            console.log(`Performing search: ${search.name}, startAt: ${startAt}, maxResults: ${maxResults}`);
+            return await this.jira.searchJira(search.jql, {startAt: startAt, maxResults: maxResults});
         } catch (error) {
-            console.error(`Error performing jira search: ${error}`)
-            throw error
+            console.error(`Error performing jira search: ${error}`);
+            throw error;
         }
     }
 
     private async getJiraSearchResults(search: Search) {
-        // TODO: A
+        const ISSUES_PER_SEARCH = 50;
+        try {
+            console.log(`Performing search '${this.jiraSearch.name}': ${this.jiraSearch.jql}`);
+            let startAt = 0;
+            let searchResults = await this.performJiraSearch(this.jiraSearch, startAt);
+            let issuesLeft = searchResults.total - ISSUES_PER_SEARCH;
+            let issues = searchResults.issues;
+            while (issuesLeft > 0) {
+                startAt += ISSUES_PER_SEARCH;
+                searchResults = await this.performJiraSearch(this.jiraSearch, startAt);
+                issues.push(...searchResults.issues);
+                issuesLeft -= ISSUES_PER_SEARCH;
+            }
+            return issues;
+        } catch (error) {
+            console.error(`Error getting Jira search results: ${error}`);
+            throw error;
+        }
+    }
+
+    private async generateReportData() {
+        try {
+            const FIELD_ASSIGNEE = this.jiraFields.byName['Assignee'].id;
+            const FIELD_ISSUE_TYPE = this.jiraFields.byName['Issue Type'].id;
+            const FIELD_STATUS = this.jiraFields.byName['Status'].id;
+            const FIELD_SUMMARY = this.jiraFields.byName['Summary'].id;
+            const FIELD_SPRINT = this.jiraFields.byName['Sprint'].id;
+            const FIELD_EPIC_LINK = this.jiraFields.byName['Epic Link'].id;
+            const FIELD_EPIC_NAME = this.jiraFields.byName['Epic Name'].id;
+            const FIELD_STORY_POINTS = this.jiraFields.byName['Story Points'].id;
+            const FIELD_TIME_SPENT = this.jiraFields.byName['Time Spent'].id;
+            let issuesByKey = this.jiraIssues.reduce((map, issue) => {
+                map[issue.key] = issue;
+                return map;
+            }, {}); 
+            let reportData = <ReportData>{};
+            let reportDataIssuesPromise = this.jiraIssues.map(async issue => {
+                let reportDataIssue = <ReportDataIssue>{};
+                reportDataIssue.issue = issue.key;
+                reportDataIssue.uri = `${this.config.jira.protocol}://${this.config.jira.host}/browse/${issue.key}`;
+                reportDataIssue.assignee = ((FIELD_ASSIGNEE in issue.fields) && issue.fields[FIELD_ASSIGNEE] && issue.fields[FIELD_ASSIGNEE].displayName);
+                reportDataIssue.issueType = ((FIELD_ISSUE_TYPE in issue.fields) && issue.fields[FIELD_ISSUE_TYPE] && issue.fields[FIELD_ISSUE_TYPE].name);
+                reportDataIssue.summary = ((FIELD_SUMMARY in issue.fields) && issue.fields[FIELD_SUMMARY]);
+                reportDataIssue.status = ((FIELD_STATUS in issue.fields) && issue.fields[FIELD_STATUS] && issue.fields[FIELD_STATUS].name);
+                reportDataIssue.storyPoints = ((FIELD_STORY_POINTS in issue.fields) && issue.fields[FIELD_STORY_POINTS]);
+                reportDataIssue.storyPointsRemaining = null;
+                if (reportDataIssue.storyPoints) {
+                    let timeSpent = ((FIELD_TIME_SPENT in issue.fields) && issue.fields[FIELD_TIME_SPENT]);
+                    if (timeSpent) {
+                        // Time spent in work days
+                        timeSpent /= (60 * 60 * 8);
+                        reportDataIssue.storyPointsRemaining = reportDataIssue.storyPoints - timeSpent;
+                    }
+                }
+                reportDataIssue.sprint = null;
+                if ((FIELD_SPRINT in issue.fields) && issue.fields[FIELD_SPRINT] && issue.fields[FIELD_SPRINT].length) {
+                    let latestSprintData = _.last(issue.fields[FIELD_SPRINT]);
+                    let sprintDataFields = /[^\[]+\[([^\]]+)\]/.exec(latestSprintData)[1].split(',');
+                    let sprint = <Sprint>{};
+                    for (let sprintDataField of sprintDataFields) {
+                        let data = sprintDataField.split('=');
+                        if (data[0] === 'name') {
+                            sprint.name = data[1];
+                        } else if (data[0] === 'id') {
+                            sprint.id = parseInt(data[1]);
+                        } else if (data[0] === 'state') {
+                            sprint.state = data[1];
+                        } else if (data[0] === 'startDate') {
+                            sprint.startDate = data[1];
+                        } else if (data[0] === 'endDate') {
+                            sprint.endDate = data[1];
+                        } else if (data[0] === 'completedDate') {
+                            sprint.completedDate = data[1];
+                        }
+                    }
+                    reportDataIssue.sprint = sprint;
+                }
+                reportDataIssue.epic = null;
+                if ((FIELD_EPIC_NAME in issue.fields) && issue.fields[FIELD_EPIC_NAME]) {
+                    reportDataIssue.epic = `${issue.key} - ${issue.fields[FIELD_EPIC_NAME]}`;
+                } else if ((FIELD_EPIC_LINK in issue.fields) && issue.fields[FIELD_EPIC_LINK]) {
+                    let epicKey = issue.fields[FIELD_EPIC_LINK];
+                    if (epicKey in issuesByKey) {
+                        let epicIssue = issuesByKey[epicKey];
+                        reportDataIssue.epic = `${epicIssue.key} - ${epicIssue.fields[FIELD_EPIC_NAME]}`;
+                    } else {
+                        let epicIssue = await this.jira.findIssue(epicKey);
+                        reportDataIssue.epic = `${epicIssue.key} - ${epicIssue.fields[FIELD_EPIC_NAME]}`;
+                    }
+                }
+                return reportDataIssue;
+            });
+            reportData.reportDataIssues = [];
+            let reportDataIssues = await Promise.all(reportDataIssuesPromise);
+            for (let reportDataIssue of reportDataIssues) {
+                reportData.reportDataIssues.push(<ReportDataIssue>reportDataIssue);
+            }
+            reportData.reportDataIssues.sort((a, b) => {
+                // Sort by sprint, active first
+                if (a.sprint || b.sprint) {
+                    if (a.sprint && !b.sprint) {
+                        return -1;
+                    } else if (!a.sprint && b.sprint) {
+                        return 1;
+                    } else {
+                        if (a.sprint.state === 'ACTIVE' && b.sprint.state !== 'ACTIVE') {
+                            return -1;
+                        } else if (a.sprint.state !== 'ACTIVE' && b.sprint.state === 'ACTIVE') {
+                            return 1;
+                        } else if (a.sprint.id !== b.sprint.id) {
+                            return a.sprint.id - b.sprint.id;
+                        }
+                    }
+                }
+
+                // Sort by assignee
+                if (a.assignee || b.assignee) {
+                    if (a.assignee && !b.assignee) {
+                        return -1;
+                    } else if (!a.assignee && b.assignee) {
+                        return 1;
+                    } else if (a.assignee !== b.assignee) {
+                        return a.assignee.localeCompare(b.assignee);
+                    }
+                }
+
+                // Sort by story points, higher first
+                if (a.storyPoints || b.storyPoints) {
+                    if (a.storyPoints && !b.storyPoints) {
+                        return -1;
+                    } else if (!a.storyPoints && b.storyPoints) {
+                        return 1
+                    } else if (a.storyPoints !== b.storyPoints) {
+                        return b.storyPoints - a.storyPoints;
+                    }
+                }
+
+                return a.issue.localeCompare(b.issue);
+            });
+            return reportData;
+        } catch (error) {
+            console.error(`Error generating report data: ${error}`);
+            throw error;
+        }
     }
 
     private async doJira() {
         try {
-            this.jiraFields = await this.getJiraFields()
+            this.jiraFields = await this.getJiraFields();
+            this.jiraIssues = await this.getJiraSearchResults(this.jiraSearch);
+            console.log(`Retrieved ${this.jiraIssues.length} issues`);
+            this.reportData = await this.generateReportData();
+            console.log(JSON.stringify(this.reportData, null, 4));
         } catch (error) {
-            console.error(`Error performing jira tasks: ${error}`)
+            console.error(`Error performing jira tasks: ${error}`);
         }
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as minimist from 'minimist'
-import { config } from './config'
+import * as minimist from 'minimist';
+import { config } from './config';
 
 interface CommandLineArguments {
     _: Array<string>,
@@ -7,37 +7,37 @@ interface CommandLineArguments {
 }
 
 function usage() {
-    console.log(`usage: npm start -- --search <searchName>`)
+    console.log(`usage: npm start -- --search <searchName>`);
 }
 
-const args = <CommandLineArguments>minimist(process.argv.slice(2))
+const args = <CommandLineArguments>minimist(process.argv.slice(2));
 
 if (!args.search) {
-    console.error('Search name not specified.')
-    usage()
-    process.exit()
+    console.error('Search name not specified.');
+    usage();
+    process.exit();
 }
 
 if (!config.searches.length) {
-    console.error('Must have searches configured in config.json')
-    usage()
-    process.exit()
+    console.error('Must have searches configured in config.json');
+    usage();
+    process.exit();
 }
 
 let jiraSearch = null
 for (let configuredSearch of config.searches) {
     if (configuredSearch.name === args.search) {
-        jiraSearch = configuredSearch
-        break
+        jiraSearch = configuredSearch;
+        break;
     }
 }
 
 if (!jiraSearch) {
-    console.error(`Search name "${args.search}" not found in config.json. Configured searches: ${config.searches.map(x => x.name).join(', ')}`)
-    usage()
-    process.exit()
+    console.error(`Search name "${args.search}" not found in config.json. Configured searches: ${config.searches.map(x => x.name).join(', ')}`);
+    usage();
+    process.exit();
 }
 
-import App from './App'
-let app = new App(config)
-app.execute()
+import App from './App';
+let app = new App(config, jiraSearch);
+app.execute();
